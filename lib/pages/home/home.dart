bfc4bb5f32b8dfe387a852c61dea1c4792e450c4
ayla syahira <aylasyafaa23@gmail.com>
2024-01-19 22:34:36 +0700

    second commit

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -3,14 +3,19 @@ import 'package:get/get.dart';
 import 'package:crud_database/pages/home/controller.dart';
 
 class HomePageView extends GetView<HomePageController> {
-  const HomePageView({super.key});
+  const HomePageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+        backgroundColor: Colors.deepPurple, // Warna latar belakang App Bar
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: controller.createNote,
         child: const Icon(Icons.add),
+        backgroundColor: Colors.deepPurple, // Warna latar belakang FAB
       ),
       body: SafeArea(
         child: Obx(
@@ -22,18 +27,23 @@ class HomePageView extends GetView<HomePageController> {
             shrinkWrap: true,
             itemCount: controller.notes.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(controller.notes[index].title),
-                subtitle: Text(controller.notes[index].content),
-                trailing: PopupMenuButton(
+              return Card(
+                elevation: 3, // Elevasi kartu
+                margin: const EdgeInsets.all(8), // Ruang antar kartu
+                child: ListTile(
+                  title: Text(controller.notes[index].title),
+                  subtitle: Text(controller.notes[index].content),
+                  trailing: PopupMenuButton(
                     itemBuilder: (BuildContext context) => [
                       const PopupMenuItem(child: Text("Edit")),
                       PopupMenuItem(
                         child: Text("Delete"),
-                        onTap: () => controller
-                            .deleteNotes(controller.notes[index].id!),
-                      )
-                    ]),
+                        onTap: () =>
+                            controller.deleteNotes(controller.notes[index].id!),
+                      ),
+                    ],
+                  ),
+                ),
               );
             },
           ),
